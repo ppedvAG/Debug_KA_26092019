ef95e403eb31892a4a70ba6089e2b46ecd832181
Andre <ruland@outlook.de>
2019-09-27 15:33:03 +0200

    TPL

---
+++ HalloTPL/HalloTPL/HalloDelegate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HalloTPL
+{
+    delegate void EinfacherDelete();
+    delegate void DeleMitPara(string teowuhfowejnfoewfofwemxt);
+    delegate long CalcDelegate(int a, int b);
+
+    class HalloDelegate
+    {
+        public HalloDelegate()
+        {
+
+            EinfacherDelete meinDele = EinfacheMethode;
+            Action meinDeleAlsAction = EinfacheMethode;
+            Action meinDeleAno = delegate () { Console.WriteLine("lala"); };
+            Action meinDeleAno2 = () => { Console.WriteLine("lala"); };
+            Action meinDeleAno3 = () => Console.WriteLine("lala");
+
+            DeleMitPara deleMitPara = MethodeMitPara;
+            Action<string> deleMitParaAlsAction = MethodeMitPara;
+            Action<string> deleMitParaAlsActionAno = (string text) => { Console.WriteLine(text); };
+            Action<string> deleMitParaAlsActionAno2 = (text) => Console.WriteLine(text); 
+            Action<string> deleMitParaAlsActionAno3 = x => Console.WriteLine(x); 
+
+            CalcDelegate calc = Sum;
+            Func<int, int, long> calcAlsFunc = Sum;
+            Func<int, int, long> calcAlsFuncAno = (x, y) => { return x + y; };
+            Func<int, int, long> calcAlsFuncAno2 = (x, y) =>  x + y;
+
+            List<string> texte = new List<string>();
+            texte.Where(x => x.StartsWith("b"));
+            texte.Where(Filter);
+        }
+
+        private bool Filter(string arg)
+        {
+            if (arg.StartsWith("b"))
+                return true;
+            else
+                return false;
+        }
+
+        private long Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void MethodeMitPara(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private void EinfacheMethode()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
---
+++ HalloTPL/HalloTPL/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloTPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Hallo TPL ***");
+
+            //Parallel.Invoke(Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle);
+            //Parallel.For(0, 1000000, i => Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {i}"));
+
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine("T1 gestartet");
+                Thread.Sleep(800);
+                Console.WriteLine("T1 fertig");
+            });
+
+            Task<long> t2 = new Task<long>(() =>
+            {
+                Console.WriteLine("T2 gestartet");
+                Thread.Sleep(700);
+                Console.WriteLine("T2 fertig");
+                //throw new FieldAccessException();
+                return 234567890;
+            });
+
+            t2.ContinueWith(tr =>
+            {
+                Console.WriteLine($" T2 ERROR: {tr.Exception.InnerException.Message}");
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Default);
+
+            t2.ContinueWith(tr =>
+            {
+                Console.WriteLine($" T2 OK: {tr.Result}");
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Default);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+            //Console.WriteLine($"{t2.Result}");
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+
+        static void Zähle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {i}");
+            }
+        }
+    }
+}
